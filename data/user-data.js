08ca518258203a844a051adbf57ce80bd733eae3
Dominik Flüchter <dominik.fluechter@googlemail.com>
2025-12-05 23:48:29 +0100

    Aktualisieren von user-data.js

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,13 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  githubUser: 'dfluechter',
+  devUsername: "dfluechter",
+  github: 'https://github.com/dfluechter',
+  facebook: 'https://www.facebook.com/dominik.fluechter/',
+  linkedIn: 'https://www.linkedin.com/in/dfluechter/',
+  twitter: 'https://twitter.com/FluchterDominik',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/',
   leetcode: "https://leetcode.com/said3812/",
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
-  skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
+  skills: ['React', 'Apache Spark', 'Python', 'R', 'Tableau', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
   timezone: '+6'
 };
